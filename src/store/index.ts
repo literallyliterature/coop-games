2836df41b45113767349e95d41a02fa4ad6bdd98
Sahitya Beri <sahityaberi@gmail.com>
2021-09-26 08:20:37 +0000

    Move a lot of logic into the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { createGame, extractSudokuCells, SudokuCell } from '@/sudoku/sudoku.utils';
 
 Vue.use(Vuex);
 
@@ -17,16 +18,49 @@ export default new Vuex.Store({
         focusCell(state, { row, col }) {
           state.focusedCell = { row, col };
         },
+        initialiseGame(state, difficulty = 'easy') {
+          const newGame = createGame(difficulty);
+          const gameString = newGame.solution;
+          const originalsString = newGame.puzzle.replaceAll('-', ' ');
+
+          state.gameString = gameString;
+          state.originalsString = originalsString;
+          state.cells = extractSudokuCells({
+            gameString,
+            originalsString,
+          });
+        },
+        setCellValue(state, { row, col, value }) {
+          state.cells[9 * row + col].userInput = value;
+        },
       },
       state: () => ({
+        cells: [],
         focusedCell: {
           row: -1,
           col: -1,
         },
         gameString: '',
-        originalInputsString: '',
-        userInputsString: '',
+        originalsString: '',
       }),
+      getters: {
+        duplicates: (state, getters) => getters.mistakes
+          .filter((mistakeCell: SudokuCell) => state.cells.some(
+            (cell: SudokuCell) => (
+              cell.userInput === mistakeCell.userInput
+              && cell.index !== mistakeCell.index
+              && (
+                cell.column === mistakeCell.column
+                || cell.row === mistakeCell.row
+                || cell.square === mistakeCell.square)
+            ),
+          )),
+        mistakes: (state) => state.cells
+          .filter((cell: SudokuCell) => cell.original === ' '
+          && cell.userInput !== cell.correctValue
+          && cell.userInput !== ' '),
+        userInputsString: (state) => state.cells.map((cell: SudokuCell) => cell.userInput).join(''),
+      },
     },
   },
 });
--- src/sudoku/sudoku.utils.ts
+++ src/sudoku/sudoku.utils.ts
@@ -1,63 +1,22 @@
-import { uniq } from 'lodash';
-
 export { getSudoku as createGame } from 'sudoku-gen';
 
-type CellRange = 0|1|2|3|4|5|6|7|8;
-type ValueRange = '1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9';
-type UserInputValueRange = ValueRange|'-';
-type SudokuCell = {
+export type CellRange = 0|1|2|3|4|5|6|7|8;
+export type ValueRange = '1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9';
+export type UserInputValueRange = ValueRange|' ';
+export type SudokuCell = {
   column: CellRange,
+  correctValue: ValueRange,
   index: number,
+  original: UserInputValueRange,
   row: CellRange,
   square: CellRange, // starting with 0|1|2 in the top row
-  value: ValueRange,
-};
-type UserInputSudokuCell = SudokuCell & {
-  originalInput: UserInputValueRange,
   userInput: UserInputValueRange,
 };
-type GameStatus = {
+export type GameStatus = {
   duplicates?: SudokuCell[],
   mistakes?: SudokuCell[],
   complete?: boolean,
 };
-
-export function analyseGameProgress({
-  gameString,
-  originalInputsString,
-  userInputsString,
-}: {
-  gameString: string,
-  originalInputsString: string,
-  userInputsString: string,
-}): GameStatus {
-  if (gameString === userInputsString) return { complete: true };
-
-  const cells = extractSudokuCells(gameString);
-  const originalInputs = originalInputsString.split('');
-  const userInputs = userInputsString.split('');
-  const userInputCells = cells.map((cell, index) => ({
-    ...cell,
-    originalInput: originalInputs[index],
-    userInput: userInputs[index],
-  })) as UserInputSudokuCell[];
-
-  const userEditableCells = userInputCells.filter((cell) => cell.originalInput === '-');
-  const mistakes = userEditableCells.filter((cell) => cell.userInput !== cell.value && cell.userInput !== '-');
-  const duplicates = mistakes.filter(
-    (mistake) => userInputCells.some(
-      (cell) => (
-        cell.userInput === mistake.userInput
-        && cell.index !== mistake.index
-        && (
-          cell.column === mistake.column
-          || cell.row === mistake.row
-          || cell.square === mistake.square)
-      ),
-    ),
-  );
-  return { duplicates, mistakes };
-}
 
 export function calculateColumnFromIndex(index: number): CellRange {
   return index % 9 as CellRange;
@@ -74,38 +33,24 @@ export function calculateSquareFromIndex(index: number): CellRange {
   return ((3 * floor(row / 3)) + floor(column / 3)) as CellRange;
 }
 
-export function findCellsWithMistakes(gameString: string): SudokuCell[] {
-  const columnDuplicates = findCellsWithDuplicatesInColumns(gameString);
-  const rowDuplicates = findCellsWithDuplicatesInRows(gameString);
-  const squareDuplicates = findCellsWithDuplicatesInSquare(gameString);
-  return uniq([
-    ...columnDuplicates,
-    ...rowDuplicates,
-    ...squareDuplicates,
-  ]);
+export function calculateIndexFromRowAndCol(row: number, col: number): number {
+  return 9 * row + col;
 }
 
-function findCellsWithDuplicatesInColumns(gameString: string): SudokuCell[] {
-  if (gameString.length) return [];
-  return [];
-}
-
-function findCellsWithDuplicatesInRows(gameString: string): SudokuCell[] {
-  if (gameString.length) return [];
-  return [];
-}
-
-function findCellsWithDuplicatesInSquare(gameString: string): SudokuCell[] {
-  if (gameString.length) return [];
-  return [];
-}
-
-export function extractSudokuCells(gameString: string): SudokuCell[] {
-  return gameString.split('').map((value, index) => ({
+export function extractSudokuCells({
+  gameString,
+  originalsString,
+}: {
+  gameString: string,
+  originalsString: string,
+}): SudokuCell[] {
+  return gameString.split('').map((correctValue, index) => ({
     column: calculateColumnFromIndex(index),
     index,
+    original: originalsString[index] as UserInputValueRange,
     row: calculateRowFromIndex(index),
     square: calculateSquareFromIndex(index),
-    value: value as ValueRange,
+    correctValue: correctValue as ValueRange,
+    userInput: originalsString[index] as UserInputValueRange,
   }));
 }
